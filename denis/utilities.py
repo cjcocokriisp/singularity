cjcocokriisp/singularity

commit c5ac0002abee663a3733567f2a3865a83ebad0a7

denis: utilities: update_tags: tag to empty commit if no sub exists

denis/utilities.py
@@ -50,6 +50,10 @@ def update_tags(assignment, component):
         repo.config_writer().set_value('user', 'name', 'denis').release()
         (repo.config_writer().set_value('user', 'email', 'denis@denis')
                              .release())
+        if 'EMPTY' not in repo.tags:
+            repo.git.commit('--allow-empty', '-m', 'No gradeable submission.')
+            repo.create_tag('EMPTY')
+
         for user in orbit.db.User.select():
             new_tag_name = f'{assignment}_{component}_{user.username}'
             if new_tag_name in repo.tags:
@@ -58,9 +62,11 @@ def update_tags(assignment, component):
                 continue
             user_sub = subs.where(grd_tbl.user == user.username).first()
             if not user_sub:
-                continue
-            msg = user_sub.status
-            to_promote = repo.tags[user_sub.submission_id]
+                msg = 'No gradeable submission'
+                to_promote = repo.tags['EMPTY']
+            else:
+                msg = user_sub.status
+                to_promote = repo.tags[user_sub.submission_id]
             repo.create_tag(new_tag_name, ref=to_promote.commit, message=msg)
 
         repo.git.push(REMOTE_NAME, tags=True)

commit c6c7e63caf7b8f18781e51a90a690775c557eb37

denis: utilities: detect and skip nonexistent tags
It is possible that an entry in the gradable db is created even though no
tag exists because the patches are so screwed up. We should consider this
case as well when deciding whether or not to use the empty tag.

Fixes: c5ac000 ("denis: utilities: update_tags: tag to empty commit if no sub exists")

denis/utilities.py
@@ -61,12 +61,12 @@ def update_tags(assignment, component):
                       f'{new_tag_name}')
                 continue
             user_sub = subs.where(grd_tbl.user == user.username).first()
-            if not user_sub:
+            if not user_sub or (id := user_sub.submission_id) not in repo.tags:
                 msg = 'No gradeable submission'
                 to_promote = repo.tags['EMPTY']
             else:
                 msg = user_sub.status
-                to_promote = repo.tags[user_sub.submission_id]
+                to_promote = repo.tags[id]
             repo.create_tag(new_tag_name, ref=to_promote.commit, message=msg)
 
         repo.git.push(REMOTE_NAME, tags=True)