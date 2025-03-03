cjcocokriisp/singularity

commit 4dfa0e40845513a2f84e31f65f79ec8bc87bd46b

orbit: conainerfile: add more packages
fixes #243

orbit/radius.py
@@ -749,6 +749,7 @@ tar \
 make \
 gcc \
 qemu-system-riscv \
+qemu-user-static-ricsv \
 binutils-riscv64-linux-gnu \
 gcc-riscv64-linux-gnu \
 bc \
@@ -769,6 +770,8 @@ cyrus-sasl-plain \
 gdb \
 diffutils \
 strace \
+man \
+man-pages \
 ;
 dnf clean all
 DNF

commit 91d26b8c7c19dd8cbaf5cbf4941bdedcb933a743

nit: actually fix typo in package name
it was so over, but alas, we are officially so back.

Fixes: 4dfa0e4 ("orbit: conainerfile: add more packages")

orbit/radius.py
@@ -749,7 +749,7 @@ tar \
 make \
 gcc \
 qemu-system-riscv \
-qemu-user-static-ricsv \
+qemu-user-static-riscv \
 binutils-riscv64-linux-gnu \
 gcc-riscv64-linux-gnu \
 bc \